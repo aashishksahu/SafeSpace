9cfae18d31a2c02309aff5257e48ac3378a118dc
AashishKSahu <krmmvinayak@hotmail.com>
2023-03-08 15:53:31 +0530

    Initial commit

---
+++ app/src/main/java/org/android/silvervault/crypto/Crypto.kt
@@ -0,0 +1,16 @@
+package org.android.silvervault.crypto
+
+import android.content.Context
+import android.net.Uri
+
+class Crypto(private var applicationContext: Context) {
+
+    fun encrypt(password: String, files: ArrayList<Uri>) {
+
+    }
+
+    fun decrypt(password: String, files: ArrayList<Uri>) {
+
+    }
+
+}
