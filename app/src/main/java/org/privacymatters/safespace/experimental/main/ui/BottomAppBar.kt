4e1e629630de42581355a8f67533101a8c53351f
aashishksahu <sahu6@protonmail.com>
2024-06-03 16:49:39 +0530

    WIP Redesign

--- app/src/main/java/org/privacymatters/safespace/experimental/main/ui/BottomAppBar.kt
+++ app/src/main/java/org/privacymatters/safespace/experimental/main/ui/BottomAppBar.kt
@@ -17,7 +17,6 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
--- app/src/main/java/org/privacymatters/safespace/experimental/main/ui/ItemList.kt
+++ app/src/main/java/org/privacymatters/safespace/experimental/main/ui/ItemList.kt
@@ -20,7 +20,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
