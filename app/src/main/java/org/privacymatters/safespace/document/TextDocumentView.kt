bc49bd77e6504b307159d792d7d30ab7012e17c2
aashishksahu <sahu6@protonmail.com>
2024-08-15 22:20:59 +0530

    ui bug fixes

--- app/src/main/java/org/privacymatters/safespace/document/TextDocumentView.kt
+++ app/src/main/java/org/privacymatters/safespace/document/TextDocumentView.kt
@@ -3,14 +3,17 @@ package org.privacymatters.safespace.document
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.NestedScrollView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.privacymatters.safespace.R
-import org.privacymatters.safespace.experimental.main.DataManager
 import org.privacymatters.safespace.depracated.lib.Reload
+import org.privacymatters.safespace.experimental.main.DataManager
 import org.privacymatters.safespace.utils.Constants
 import org.privacymatters.safespace.utils.SetTheme
 import org.privacymatters.safespace.utils.Utils
@@ -20,9 +23,16 @@ import java.io.FileNotFoundException
 import java.io.FileReader
 import java.io.IOException
 
+enum class ScrollDirection {
+    GO_UP, GO_DOWN
+}
+
 class TextDocumentView : AppCompatActivity() {
 
     private lateinit var textFileContentView: EditText
+    private lateinit var scrollTo: ImageButton
+    private lateinit var scrollView: NestedScrollView
+    private var scrollDirection = ScrollDirection.GO_DOWN
     private lateinit var file: File
     private val ops = DataManager
 
@@ -45,7 +55,26 @@ class TextDocumentView : AppCompatActivity() {
         // will reload (to prevent clearing of selected items during app switching)
         Reload.value = true
 
+        scrollView = findViewById(R.id.scrollView2)
+        scrollTo = findViewById(R.id.scrollTo)
         textFileContentView = findViewById(R.id.textView)
+
+        scrollView.setOnScrollChangeListener(({ _, _, y, _, oldY ->
+            if ((y - oldY) >= 0) { // going down
+                scrollDirection = ScrollDirection.GO_UP
+                scrollTo.setImageResource(R.drawable.keyboard_arrow_up_24dp_e8eaed_fill0_wght400_grad0_opsz24)
+            } else { // going up
+                scrollDirection = ScrollDirection.GO_DOWN
+                scrollTo.setImageResource(R.drawable.keyboard_arrow_down_32dp_e8eaed_fill0_wght400_grad0_opsz40)
+            }
+        }))
+
+        scrollTo.setOnClickListener {
+            when (scrollDirection) {
+                ScrollDirection.GO_UP -> scrollView.fullScroll(NestedScrollView.FOCUS_UP)
+                ScrollDirection.GO_DOWN -> scrollView.fullScroll(NestedScrollView.FOCUS_DOWN)
+            }
+        }
 
         file = File(intent.extras?.getString(Constants.INTENT_KEY_PATH)!!)
 
@@ -67,7 +96,7 @@ class TextDocumentView : AppCompatActivity() {
 
         } catch (e: FileNotFoundException) {
 
-            Utils.exportToLog(application,"@TextDocumentView.onCreate()", e)
+            Utils.exportToLog(application, "@TextDocumentView.onCreate()", e)
 
             val builder = MaterialAlertDialogBuilder(textFileContentView.context)
 
@@ -81,7 +110,7 @@ class TextDocumentView : AppCompatActivity() {
             val alert = builder.create()
             alert.show()
         } catch (e: IOException) {
-            Utils.exportToLog(application,"@TextDocumentView.onCreate()", e)
+            Utils.exportToLog(application, "@TextDocumentView.onCreate()", e)
             val builder = MaterialAlertDialogBuilder(textFileContentView.context)
 
             builder.setTitle(getString(R.string.text_exception_IO))
@@ -111,7 +140,7 @@ class TextDocumentView : AppCompatActivity() {
 
         } catch (e: Exception) {
 
-            Utils.exportToLog(application,"@TextDocumentView.saveFile()", e)
+            Utils.exportToLog(application, "@TextDocumentView.saveFile()", e)
 
             Toast.makeText(
                 applicationContext,
